890dd6e7bfce7afc6f7c83a0141a5783d19caebe
Peter <peter@Peters-Air.lan>
2022-03-31 15:34:22 -0500

    removed blog link from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,14 +79,6 @@ function Navbar() {
               </li>
               <li>
                 <a
-                  href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0 dark:text-gray-400 md:dark:hover:text-teal dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a
                   href="/Contact"
                   class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0 dark:text-gray-400 md:dark:hover:text-teal dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
