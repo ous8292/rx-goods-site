4b36c9c67cf563f66d38c3d06c0d4eb5686c216c
Peter <peter@Peters-Air.lan>
2022-05-02 14:22:43 -0500

    added image grid styling css file and map function to display images on page

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import Images from '../api-mock.json';
+import React, { useState } from 'react';
+import images from '../api-mock.json';
+import '../styles/Gallery.css';
 
 function Gallery() {
-  console.log(Images);
-  return <div>Hello!</div>;
+  //initilizing state hook with our images from api data
+  const [imageList, setImageList] = useState(images.resources);
+
+  return (
+    <div className="image-grid">
+      {imageList.map(image => (
+        <img src={image.url} alt={image.public_id}></img>
+      ))}
+    </div>
+  );
 }
 
 export default Gallery;
