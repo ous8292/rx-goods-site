fc58f632c42f68ca93e1743d37eed91eb6485d8b
Peter <peter@Peters-Air.lan>
2022-05-06 13:05:11 -0500

    added logic, button, and styling to load more pictures/pagination

--- src/API.js
+++ src/API.js
@@ -1,7 +1,11 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
-export const getImages = async () => {
-  const response = await fetch(`${API_URL}/photos`);
+export const getImages = async nextCursor => {
+  const params = new URLSearchParams();
+  if (nextCursor) {
+    params.append('next_cursor', nextCursor);
+  }
+  const response = await fetch(`${API_URL}/photos?${params}`);
   const responseJson = await response.json();
   return responseJson;
 };
--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -6,21 +6,40 @@ import '../styles/Gallery.css';
 function Gallery() {
   //initilizing state hook with our images from api data
   const [imageList, setImageList] = useState([]);
+  const [nextCursor, setNextCursor] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const responseJson = await getImages();
       setImageList(responseJson.resources);
+      //next_cursor is inside the response object
+      setNextCursor(responseJson.next_cursor);
     };
     fetchData();
   }, []);
 
+  const handleLoadMoreButtonClick = async () => {
+    const responseJson = await getImages(nextCursor);
+    setImageList(currentImageList => [
+      ...currentImageList,
+      ...responseJson.resources,
+    ]);
+    setNextCursor(responseJson.next_cursor);
+  };
+
   return (
-    <div className="image-grid">
-      {imageList.map(image => (
-        <img src={image.url} alt={image.public_id}></img>
-      ))}
-    </div>
+    <>
+      <div className="image-grid">
+        {imageList.map(image => (
+          <img src={image.url} alt={image.public_id}></img>
+        ))}
+      </div>
+      <div className="load-button">
+        {nextCursor && (
+          <button onClick={handleLoadMoreButtonClick}> Load More</button>
+        )}
+      </div>
+    </>
   );
 }
 
